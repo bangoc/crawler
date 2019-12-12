23eb9e8154d335c9ecb370bbc43a128fec876355
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2019-12-12 08:43:29 +0700

    Add location name to the output message of max pm25 (moitruongthudo)

--- moitruongthudo/max_aqi_pm25.js
+++ moitruongthudo/max_aqi_pm25.js
@@ -2,8 +2,21 @@ var rp = require("request-promise");
 var max_pm25_aqi = 0;
 var max_pm25_site_id = "";
 var max_pm25_time = "";
-async function getData() {
-  for (let id of ["1", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]) {
+var sites = new Map(
+              [[1, "Hoàn Kiếm"],
+               [7, "Thành Công"],
+               [8, "Tân Mai"],
+               [9, "Kim Liên"],
+               [10, "Phạm Văn Đồng"],
+               [11, "Tây Mỗ"],
+               [12, "Mỹ Đình"],
+               [13, "Hàng Đậu"],
+               [14, "Chi cục bảo vệ môi trường"],
+               [15, "Minh Khai - Bắc Từ Liêm"],
+               [16, "French Embassy in Vietnam"]
+               ]);
+async function findMaxPM25() {
+  for (let id of sites.keys()) {
     var options = {
       uri: 'http://moitruongthudo.vn/public/dailyaqi/PM2.5?site_id=' + id,
       headers: {
@@ -24,8 +37,9 @@ async function getData() {
   }
 }
 (async () => {
-  await getData();
+  await findMaxPM25();
   console.log("Max AQI by PM2.5:\n\tValue: " + max_pm25_aqi +
               "\n\tsite_id: " + max_pm25_site_id +
+              "\n\tsite_name: " + sites.get(max_pm25_site_id) +
               "\n\ttime: " + max_pm25_time);
 })();
